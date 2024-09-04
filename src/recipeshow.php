4dc7229796215de9ad19d6af16e13e9da5f166f4
yooneiphyu11 <yphyu49@gmail.com>
2024-09-05 00:51:10 +0630

    yep

--- src/recipeshow.php
+++ src/recipeshow.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>recipeshow</title>
  <!-- bootstrap css link -->
  <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
  <!-- css link -->
