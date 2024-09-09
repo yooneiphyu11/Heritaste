e514f8de4503c32bd613c01af36e58b72a695076
yooneiphyu11 <yphyu49@gmail.com>
2024-09-09 10:18:54 +0630

    admin1

--- src/index.php
+++ src/index.php
@@ -33,16 +33,6 @@ $random_my=$CRUD->random_my();
             </div>
 
             <div class="d-flex align-item-center me-5 order-lg-2">
-            <form id="languageForm " class="me-5 mt-1" style="border-radius: 20px;">
-                <div class="button-switch-container">
-                    <select id="languageSelect" class="language" aria-label="Select language" title="Select language"
-                    style="border: 2px solid #2b7067; border-radius: 15px; padding: 5px 10px; font-size: 16px; margin-right: 10px;">
-                    <option value="en">EN</option>
-                    <option value="my">MY</option>
-                    </select>
-                    <button type="submit" id="submitBtn" style="display: none;"></button>
-                </div>
-            </form>
                 <a class="nav-link me-5" href="#">
                     <img src="../src/assets/images/moon_4139162.png" alt=""  style="width: 30px; height: 30px;" id="icon">
                 </a>
